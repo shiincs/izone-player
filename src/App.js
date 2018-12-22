58c5e01cf56df322e41b3015fec00614a655f2b0
Shin Changseon <skywing27@gmail.com>
2018-12-22 23:54:45 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classNames from "classnames/bind";
+import styles from "./App.module.scss";
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className={cx("header")}>
+        <h1>아이즈원 플레이어</h1>
+      </header>
     );
   }
 }
