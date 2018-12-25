d0744eef895e8b32fa4d2a39710c0a8ca704dea4
Shin Changseon <skywing27@gmail.com>
2018-12-25 23:34:05 +0900

    import BrowserRouter for using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import styles from './App.module.scss';
 import LayoutView from './components/LayoutView/LayoutView';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
     return (
-      <LayoutView>
-        <main>아이즈원 한정 영상 저장소</main>
-      </LayoutView>
+      <Router>
+        <LayoutView>
+          <main>아이즈원 한정 영상 저장소</main>
+        </LayoutView>
+      </Router>
     );
   }
 }
