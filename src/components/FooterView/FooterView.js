3d94c0c83efb6607cd9a3a0382a906fb140ebf3a
Shin Changseon <skywing27@gmail.com>
2018-12-26 01:21:48 +0900

    style: edit header,footer layout & styling

--- src/components/FooterView/FooterView.js
+++ src/components/FooterView/FooterView.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
+import styles from './FooterView.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default class FooterView extends Component {
   render() {
-    return <footer>This Webpage made by Changseon</footer>;
+    return (
+      <footer className={cx('footer')}>
+        <p className={cx('copyright')}>&copy; 2019 izone-player.netlify.com</p>
+      </footer>
+    );
   }
 }
--- src/components/HeaderView/HeaderView.js
+++ src/components/HeaderView/HeaderView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import styles from './HeaderView.module.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Hamburger } from '../../commons/hamburger.svg';
 
 const cx = classNames.bind(styles);
 
@@ -8,7 +10,79 @@ export default class HeaderView extends Component {
   render() {
     return (
       <header className={cx('header')}>
-        <h1>아이즈원 플레이어</h1>
+        <h1 className={cx('title')}>
+          <Link to='/'>아이즈원 플레이어</Link>
+        </h1>
+
+        <ul className={cx('userMenu')}>
+          <li className={cx('loginBtn')}>
+            <Link to='/'>로그인</Link>
+          </li>
+
+          <li className={cx('registerBtn')}>
+            <Link to='/'>회원가입</Link>
+          </li>
+        </ul>
+
+        <nav className={cx('navMenu')}>
+          <ul className={cx('navList')}>
+            <li className={cx('navItem')}>
+              <Link to='/'>전체</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>장원영</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>미야와키 사쿠라</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>조유리</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>최예나</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>안유진</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>야부키 나코</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>권은비</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>강혜원</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>혼다 히토미</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>김채원</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>김민주</Link>
+            </li>
+
+            <li className={cx('navItem')}>
+              <Link to='/'>이채연</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <button className={cx('hamburger')}>
+          <Hamburger />
+        </button>
       </header>
     );
   }
